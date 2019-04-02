0b0ab63bc12c6b2217e68661bde3bab4d78cc1b1
jiananshi <klamtine@gmail.com>
2019-04-02 17:45:45 +0800

    $feat: add hottopics

--- pages/index/index.ts
+++ pages/index/index.ts
@@ -9,26 +9,34 @@ Page({
     },
     post: {
       list: [],
+      listCopy: [],
       page: 1,
       num: 20
-    }
+    },
+    topics: []
   },
   onLoad() {
     const swiper = this.selectComponent('#swiper');
     const { page, num, list } = this.data.post;
     Promise.all([
       request('home/forums', 'get', {}, true),
-      request('home/mobileHomeInfo', 'get', { page: 1, num: 20 }, true)
-    ]).then(([ nav, info ]) => {
+      request('home/mobileHomeInfo', 'get', { page: 1, num: 20 }, true),
+      request('topic/getRecommendTopicList', 'get', {}, true)
+    ]).then(([ nav, info, topics ]) => {
       list.push(...info.hots);
+      info.hots.forEach(hot => {
+        hot.imgsCover = hot.imgs && hot.imgs.length && hot.imgs.slice(0, 3);
+      });
       this.setData({ 
         ['nav.list']: nav.forumlists,
         sliders: info.circles,
         post: {
           list,
+          listCopy: list.slice(2),
           page: page + 1,
           num
-        }
+        },
+        topics: topics.list
       });
       swiper.start();
     })
