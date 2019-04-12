569b40c97d71edd0e6d53efe98503fce37f4b035
jiananshi <klamtine@gmail.com>
2019-04-12 17:21:29 +0800

    $chore: update gitignore

---
+++ serverless/proxy/index.js
@@ -0,0 +1,17 @@
+const cloud = require('wx-server-sdk');
+const req = require('request-promise');
+
+cloud.init();
+
+exports.main = async (event, context) => {
+  const { url, method, data } = event;
+  return await req({
+    uri: url,
+    method: method.toUpperCase(),
+    [method === 'get' ? 'qs' : 'body']: data,
+    headers: {
+      'Content-Type': 'application/json',
+      referer: 'https://m.bbs.mihoyo.com/bh3'
+    }
+  });
+};
